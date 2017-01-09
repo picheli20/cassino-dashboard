7b82914dc0ebe41bcb772a1fdfe19b43a9dff011
Roberto Picheli <fopl@gft.com>
2017-01-09 15:27:45 -0200

    Fixing route value

--- src/app/pages/game/game.ts
+++ src/app/pages/game/game.ts
@@ -1,18 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+interface IParams {
+	id : number;
+}
 
 @Component({
   selector: 'game',
   template: require('./game.html')
 })
+
 export class GameComponent {
 	id : number;
 
 	constructor(private route: ActivatedRoute) {}
 
 	ngOnInit() {
-		this.route.params.subscribe(params => {
-			this.id = params['id'];
+		this.route.params.subscribe((params : IParams) => {
+			this.id = params.id;
 		});
 	}
 }
