3d6e642640c1766dc69ea71254186f48df04f76c
Fábio Picheli <Fbio Picheli>
2017-01-10 20:15:47 -0200

    I had to move to phantomJS :(

--- conf/karma.conf.js
+++ conf/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
       outputDir: 'test-reports'
     },
     browsers: [
-      'Chrome'
+      'PhantomJS'
     ],
     frameworks: [
       'jasmine'
@@ -37,7 +37,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-junit-reporter'),
       require('karma-coverage'),
-      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-webpack')
     ]
   };
